2642c6b527576b547f2961c3aaf2ee69020149e7
JaeranKIM <devjaeran@gmail.com>
2023-02-27 17:49:39 +0900

    프로그래머스 - 약수의 개수와 덧셈 풀이 완료

--- studyAlgorithm/february/230225.js
+++ studyAlgorithm/february/230225.js
@@ -13,15 +13,35 @@ a부터 b까지 수들 중에서
 function solution(left, right) {
     var answer = 0;
     let count = 0;
+    //left부터 right까지 반복
     for(let i = left; i<=right; i++) {
+        // j가 2부터 left-1까지 반복
         for(let j=2; j<i; j++){
-            
+            //left % 2가 0이면 약수
+            if(i % j === 0) {
+                //약수의 개수 만큼 count가 쌓임
+                //count = count + 1
+                count += 1
+            }
+        //left의 약수를 찾고 count된 숫자가 아래 조건문으로 내려감
         }
+
+        //count는 약수의 개수
+        //약수의 개수 % 2가 0이면 짝수이므로 더하기
+        if(count % 2 === 0) {
+            answer += i
+        //약수의 개수 % 2가 0이 아니면 홀수이므로 빼기
+        } else {
+            answer -= i
+        }
+        //count를 0으로 초기화 해주어야 다음 left값의 count가 0부터 시작됨
+        count = 0
     }
     return answer;
 }
 
 console.log(solution(13, 17)) //43
+console.log(solution(24, 27)) //52
 
 /**
 1. 약수 구하기 -> i % 2 === 0이면 약수
