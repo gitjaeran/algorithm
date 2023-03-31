163304dd2ca2302962f671c3ab53f04cc712dbd8
JaeranKIM <devjaeran@gmail.com>
2023-03-31 21:36:06 +0900

    프로그래머스 - 예산 - 풀이중

---
+++ studyAlgorithm/march/230331.js
@@ -0,0 +1,9 @@
+//예산
+
+function solution(d, budget) {
+  var answer = 0;
+  return answer;
+}
+
+console.log([1, 3, 2, 5, 4], 9); //3
+//https://school.programmers.co.kr/learn/courses/30/lessons/12982
